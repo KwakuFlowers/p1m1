ad682af171d1cd090d5c0ec7b3107a3a4216877d
KwakuFlowers <kwakuflowers@gmail.com>
2022-04-29 20:15:04 -0400

    test to songlists

---
+++ README.md
@@ -0,0 +1,6 @@
+# milestone1-kflowers9
+1)For API access to Spotify:
+
+-Create a .env file in your root directory, and create a variable named (exactly) "CLIENT_ID" and "CLIENT_SECRET"
+-Go to Spotify Devlopers page, login, and create your app with Spotify and put your client ID and Secret from the dashboard into your .env file for each variable respectively. 
+-REMEMBER!: Put .env file under your .gitignore file 
---
+++ Spot.py
@@ -0,0 +1,47 @@
+from json import load
+import os
+from urllib import response
+import requests
+import json
+import base64
+import string
+import random
+import flask
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv())
+
+GetReqURL = "https://api.spotify.com/v1"
+api_token_url = "https://accounts.spotify.com/api/token"
+
+
+def artistsongs(artid):
+    Art_song_url = f"{GetReqURL}/artists/{artid}/top-tracks"
+
+    head = {
+        "Authorization": f"Bearer {authorize()}",
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+    }
+
+    songs = requests.get(Art_song_url, headers=head)
+    songinfo = songs.json()
+    songlist = songinfo["tracks"]
+    print(songlist)
+
+
+def authorize():
+    CLIENT_ID = os.getenv("CLIENT_ID")
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    DATA = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "grant_type": "client_credentials",
+    }
+
+    authkeydata = requests.post(
+        api_token_url, auth=(CLIENT_ID, CLIENT_SECRET), data=DATA
+    )
+    reqkey = authkeydata.json()
+    token = reqkey.get("access_token")
+    # print(token)
+    return f"{token}"
