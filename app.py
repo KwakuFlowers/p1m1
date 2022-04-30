1d5e65f52f35e4696906e8a876cea8c0d546fb53
KwakuFlowers <kwakuflowers@gmail.com>
2022-04-30 12:04:41 -0400

    App.py incorrect test

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import flask
 
+from Spot import artistsongs
+
 app = flask.flask(__name__)
 
 
 @app.route("/")
 def main():
+    artistsongs() = (
+        randsonginfo_name,
+        randsonginfo_releasedate,
+        randsonginfo_popularity,
+        randsonginfo_extern,
+        randsonginfo_imageurl,
+        randsonginfo_imageH,
+        randsonginfo_imageW,
+    ) 
     return flask.render_template("main.html")
-
+    
 
 app.run()
