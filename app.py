99409701a8323647a7fc0e47125f4038c4e4f0df
KwakuFlowers <kwakuflowers@gmail.com>
2022-04-30 20:21:12 -0400

    hopeful cahnges that'll work

--- app.py
+++ app.py
@@ -44,6 +44,12 @@ def main():
     )
 
 
-app.run(
-    host=os.getenv("IP", "0.0.0.0"), port=os.getenv("PORT", 5000), debug=False #uncomment when sending to heroku
-)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+# app.run(
+#    host=os.getenv("IP", "0.0.0.0"), port=port, debug=False #uncomment when sending to heroku
+# )
+
+# os.getenv("PORT", 5000)
